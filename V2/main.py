674b59a2d0b84125a6bf7ab0830ecb1ed55b178c
Achraf TAFFAH <a.taffah@etu.enset-media.ac.ma>
2023-05-23 06:09:28 +0200

    _

--- V2/main.py
+++ V2/main.py
@@ -1,27 +1,7 @@
 import cv2
 import numpy as np
 import streamlit as st
-
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-
-data = pd.read_csv('data/train.csv')
-data = np.array(data)
-m, n = data.shape
-np.random.shuffle(data)  # shuffle before splitting into dev and training sets
-
-data_dev = data[0:1000].T
-Y_dev = data_dev[0]
-X_dev = data_dev[1:n]
-X_dev = X_dev / 255.
-
-data_train = data[1000:m].T
-Y_train = data_train[0]
-X_train = data_train[1:n]
-X_train = X_train / 255.
-_, m_train = X_train.shape
-Y_train
 
 def ReLU(Z):
     return np.maximum(Z, 0)
@@ -45,7 +25,7 @@ def make_predictions(X, W1, b1, W2, b2):
     predictions = get_predictions(A2)
     return predictions
 
-def load_modal_2l():
+def load_model_2l():
     W1 = np.load('model/W1.npy')
     b1 = np.load('model/b1.npy')
     W2 = np.load('model/W2.npy')
@@ -55,31 +35,16 @@ def load_modal_2l():
 def load_image(image):
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     resized_image = cv2.resize(image, (28, 28))
-    flattened_image = resized_image.flatten() / 255.0
+    flattened_image = resized_image.flatten() / 255.0  # Normalize the image
     return flattened_image
 
-W1, b1, W2, b2 = load_modal_2l()
+W1, b1, W2, b2 = load_model_2l()
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
-def test_prediction(index):
-    current_image = X_train[:, index-1, None]
-    prediction = make_predictions(X_train[:, index, None], W1, b1, W2, b2)
-    label = Y_train[index]
-    print("Prediction: ", prediction)
-    print("Label: ", label)
-
-    current_image = current_image.reshape((28, 28)) * 255
-    plt.gray()
-    plt.imshow(current_image, interpolation='nearest')
-    st.pyplot()  # Display the plot using st.pyplot() instead of plt.show()
-
-test_prediction(0)
-
-uploaded_file = st.file_uploader("Choose an image file", type="jpg")
+uploaded_file = st.file_uploader("Choose an image file", type="png")
 
 if uploaded_file is not None:
-    W1, b1, W2, b2 = load_modal_2l()
     file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
     opencv_image = cv2.imdecode(file_bytes, 1)
     st.image(opencv_image, width=400)
@@ -87,7 +52,6 @@ if uploaded_file is not None:
     input_image = image.reshape((784, 1))
 
     if st.button('Predict'):
-        print("Input Image Shape: ", input_image.shape)
-        print("Input Image Range: ", np.min(input_image), np.max(input_image))
         prediction = make_predictions(input_image, W1, b1, W2, b2)
-        print("Prediction: ", prediction)
+        predicted_label = prediction[0]  # Get the predicted label (assuming make_predictions returns a single prediction)
+        st.write("Predicted Label: ", predicted_label)
